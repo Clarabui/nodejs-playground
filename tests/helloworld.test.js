c5a59f6ad1470e0d3a69a82739326cce8fd306c7
Tram Bui <trambui@Trams-MacBook-Pro.local>
2021-09-06 19:58:52 -0400

    add failing test

--- tests/helloworld.test.js
+++ tests/helloworld.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 0 + 0 to equal 0', () => {
   expect(sum(0, 0)).toBe(0);
 });
+
+test('add 10 + 0 to not equal 11', () => {
+	expect(sum(10, 0)).not.toBe(10);
+});
